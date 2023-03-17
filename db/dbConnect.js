e2f4938a8372f68e58c9fbacb08d7712008b4ba6
Andreq <andrew.avtomonov@videinfra.com>
2023-03-17 08:07:03 +0300

    mongo key

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -6,7 +6,7 @@ async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(
-        process.env.DB_URL,
+        'mongodb+srv://andrew:av54f68nr@cluster0.kd2grno.mongodb.net/?retryWrites=true&w=majority',
       {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
